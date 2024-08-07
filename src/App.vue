f4ed71605278643b067be9565643cf01976a1406
essoungou.wonga <essoungou.wonga@lao-sarl.cm>
2024-08-07 18:05:48 +0100

    ok

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,15 @@
 <template>
+  <header>
+    <nav>
+      <RouterLink to="/">
+        <img src="/youtube.png" alt="Youtube logo">
+      </RouterLink>
+      <form>
+        <input type="text" placeholder="Search...">
+      </form>
+      <font-awesome-icon icon="magnifying-glass" />
+    </nav>
+  </header>
   <RouterView />
 </template>
 
